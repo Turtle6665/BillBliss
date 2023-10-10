070751ade9628dd2901647d5a368478bbf04ca10
Diego <diego.neve@outlook.be>
2023-10-10 23:04:38 +0200

    Adding Bills push

--- js/dashboard.js
+++ js/dashboard.js
@@ -20,7 +20,7 @@ function updateInfo(){
   return fetch(apiUrl, {
     method: 'GET',
     headers: {
-      'Authorization': 'Bearer ' + token // Replace with your project ID and private code
+      'Authorization': 'Bearer ' + token
     }
   })
   .then(response => {
@@ -45,9 +45,7 @@ function updateInfo(){
     membersList.innerHTML = '';
     data.members.forEach(member => {
        const memberDiv = document.createElement('div');
-       Object.assign(memberDiv, {"textContent":member.name,"id":member.id,  "onclick":"function() {editMember(member.id)}", "style":"cursor: pointer;"})
-       memberDiv.onclick = function() {editMember(member.id)};
-       //memberDiv.textContent = `${member.name}`;
+       Object.assign(memberDiv, {"textContent":member.name,"id":member.id,  "onclick":function() {editMember(member.id)}, "style":"cursor: pointer;"})
        let img_edit = document.createElement('img');
        Object.assign(img_edit, {"src":"pen.svg", "style":"height:0.8em; display:inline-block; margin:auto auto;"})
        memberDiv.appendChild(img_edit);
@@ -67,7 +65,7 @@ function updateBills(){
   return fetch(apiUrl+"/bills", {
     method: 'GET',
     headers: {
-      'Authorization': 'Bearer ' + token // Replace with your project ID and private code
+      'Authorization': 'Bearer ' + token
     }
   })
   .then(response => {
@@ -84,7 +82,7 @@ function updateBills(){
     allbills = bills;
     bills.forEach(bill => {
         const billdiv = document.createElement('div');
-        billdiv.classList = "billdiv"
+        Object.assign(billdiv, {"classList":"billdiv","id":"bill~"+bill.id, "onclick":function(){editBill(bill.id)}});
         const name = document.createElement("div");
         Object.assign(name, {"textContent":bill.what, "classList":"billWhat"});
         billdiv.appendChild(name);
@@ -108,7 +106,7 @@ function updateBills(){
   });
 }
 
-//get the settelment
+//get the settlement
 function updateSummary(){
 
 }
@@ -116,29 +114,34 @@ function updateSummary(){
 
 //edit the informations of the project
 function editMember(MemberID){
-  console.log(MemberID)
-}
+  console.log(MemberID);
+};
 
 function editBill(BillID){
-  console.log(BillID)
-}
+  //console.log(BillID);
+  const bill = allbills.filter(bill => {return bill.id===BillID})[0]; //select the bill
+  addBill(edit=true,BillID=bill.id,what = bill.what, much=bill.amount,
+          date=bill.date,who=bill.payer_id,whom=bill.owers.reduce((a,b) => a.concat(b.id),[]),
+          currency=bill.original_currency,external_link=bill.external_link);
 
-let lastwho = ""
-function addBill(what = "", much=""){
+};
+
+let lastwho = "";
+function addBill(edit=false,BillID="",what = "", much="", date="",who="",whom="",currency="",external_link=""){
   document.getElementById("newBillPage").classList.remove("hidden");
-  document.getElementById("bill-when").value=new Date().toISOString().split('T')[0]
+  document.getElementById("bill-when").value= (date=="")? new Date().toISOString().split('T')[0] : date //select the provided date and if no, the current date
   document.getElementById("bill-what").value=what
   document.getElementById("bill-much").value=much
 
-  const who = document.getElementById("bill-who");
-  who.innerHTML="";
+  const who_input = document.getElementById("bill-who");
+  who_input.innerHTML="";
   const forWhom = document.getElementById("bill-forWhom");
   forWhom.innerHTML = "";
   info.members.forEach(member =>{
     //adding all member to who
     const memberSelect = document.createElement("option");
     Object.assign(memberSelect,{"textContent":memberNames[member.id], "value": member.id, "id":"who~"+member.id});
-    who.appendChild(memberSelect);
+    who_input.appendChild(memberSelect);
     //adding all member to whom
     const memberInput = document.createElement("input");
     Object.assign(memberInput,{"type":"checkbox","id":"whom~"+member.id, "name":"whom~"+member.id, "checked":true});
@@ -147,13 +150,56 @@ function addBill(what = "", much=""){
     Object.assign(memberLabel,{"htmlFor":"whom~"+member.id, "textContent":memberNames[member.id]});
     forWhom.appendChild(memberLabel);
   });
-  who.value=lastwho //select same as last for who payed the bill
+  who_input.value=(who=="")? lastwho:who //select same as last for who payed the bill or the who if provided
+  if(whom!=""){
+    info.members.forEach(member =>{document.getElementById("whom~"+member.id).checked=false}) //reset all checked
+    whom.forEach(memberid => {document.getElementById("whom~"+memberid).checked=true}) //select only the required checked
+  };
+  // change the buttons if we edit a bill rather than creating a new one
+  if(edit){
+    [...document.getElementsByClassName("editBill")].forEach(buttons => {buttons.classList.remove("hidden")});
+    [...document.getElementsByClassName("newBill")].forEach(buttons => {buttons.classList.add("hidden")});
+    document.getElementById("btn-UpdateBill").onclick=function(){updateBill(BillID)};
+    document.getElementById("btn-RemoveBill").onclick=function(){removeBill(BillID)};
+  }else{
+    [...document.getElementsByClassName("editBill")].forEach(buttons => {buttons.classList.add("hidden")});
+    [...document.getElementsByClassName("newBill")].forEach(buttons => {buttons.classList.remove("hidden")});
+  }
+
 }
 
+let lastbillID = 0
 function pushBill(addNew = false){
-  document.getElementById('newBillPage').classList.add('hidden')
-  lastwho = 1
-  if(addNew){addBill()}
+  const billInputData = [...document.getElementById('newBillPage').getElementsByTagName('input'),
+                         ...document.getElementById('newBillPage').getElementsByTagName('select')].reduce((one,nex)=> {
+                           if (nex.type==="checkbox"){
+                             one["payed_for"].push(nex.name.split("~")[1])
+                           }else{
+                             one[nex.name.split("-")[1]] = nex.value;
+                           }
+                           return one;
+                         },{"payed_for":[]})
+
+  fetch(apiUrl+"/bills", {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer ' + token,
+      'Content-Type': 'application/json'
+    },
+    body : JSON.stringify(billInputData)
+  }).then(response => {
+      console.log(response)
+      if (response.status === 201) {
+          return response.json(); // Parse the response JSON
+      } else {
+          throw new Error('Failed to fetch bills. Please check your credentials.');
+      }
+  }).then(data =>{
+    lastbillID = data
+    lastwho = billInputData["payer"]
+    document.getElementById('newBillPage').classList.add('hidden')
+    if(addNew){addBill()}
+  })
 }
 
 //the main run for all
